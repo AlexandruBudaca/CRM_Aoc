7d152c8d89ec4d91678f1be4c2437b9a2754d339
Alex <alex.andrul10@gmail.com>
2020-11-18 14:15:17 +0000

    nothing

--- routes/users.js
+++ routes/users.js
@@ -81,7 +81,7 @@ router.post("/login", (req, res) => {
           if (err) return res.status(400).send(err);
           res
             .cookie("auth", user.token, { secure: true, httpOnly: true })
-            .send({
+            .json({
               isAuth: true,
               id: user._id,
               email: user.email,
