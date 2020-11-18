02def8ef962ac6a621a1d3ee449906a9fa6eb56a
Alex <alex.andrul10@gmail.com>
2020-11-18 14:27:30 +0000

    secure

--- routes/users.js
+++ routes/users.js
@@ -79,11 +79,13 @@ router.post("/login", (req, res) => {
         }
         user.generateToken((err, user) => {
           if (err) return res.status(400).send(err);
-          res.cookie("auth", user.token).json({
-            isAuth: true,
-            id: user._id,
-            email: user.email,
-          });
+          res
+            .cookie("auth", user.token, { secure: true, httpOnly: true })
+            .json({
+              isAuth: true,
+              id: user._id,
+              email: user.email,
+            });
         });
       });
     });
