28fc969131dee7761f79b1a5392c751a88c5fc9f
Alex <alex.andrul10@gmail.com>
2020-11-23 20:08:01 +0000

    change origin

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  cors({ credentials: true, origin: "https://dashboaraoc.netlify.app/" })
-);
+app.use(cors({ credentials: true, origin: true }));
 
 const db = require("./config/keys").mongoURI;
 
